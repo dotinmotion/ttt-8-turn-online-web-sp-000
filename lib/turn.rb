2484bd0fbbb54b081f9ed6fcec63860dc251d7c3
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-03 22:15:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,8 +42,7 @@ def turn(board)
     input_to_index(user_input)
     if valid_move?(board, index)
       move(board, index, player="X")
-      turn(board)
-    else 
+    else turn(board)
     end
   end
   display_board(board)
